656de48792561f690a78fa1e6ec47ace56d9a5ad
Andrea Ferrari <andreaferraridev@gmail.com>
2024-01-17 11:28:29 +0100

    asd

--- react2/src/main.jsx
+++ react2/src/main.jsx
@@ -1,16 +1,21 @@
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
-const navbar = (
-  <nav>
+const mainContent = (
+  <main>
     <h1>React</h1>
+    <p>React is a JavaScript library for building user interfaces.</p>
     <ul>
-      <li>Home</li>
-      <li>About</li>
-      <li>Contact</li>
+      <li>React</li>
+      <li>Angular</li>
+      <li>Vue</li>
     </ul>
-  </nav>
+    <ul>
+      <li>Front</li>
+      <li>End</li>
+      <li>Development</li>
+    </ul>
+  </main>
 );
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(navbar);
+root.render(mainContent);
