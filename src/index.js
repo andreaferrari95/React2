d11ac24ae0c5db31d9decc2af56ae82a4eae8609
Andrea Ferrari <andreaferraridev@gmail.com>
2024-01-17 11:16:32 +0100

    asd

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import { createRoot } from "react-dom/client";
-import "./styles/main.scss";
-
-// Clear the existing HTML content
-document.body.innerHTML = '<div id="app"></div>';
-
-// Render your React component instead
-const root = createRoot(document.getElementById("app"));
-root.render(<h1>Hello, world</h1>);
